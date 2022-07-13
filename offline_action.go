5c36c06970ac11b8a699d65fe8fa25535a7e7341
krishpranav <krisna.pranav@gmail.com>
2022-07-13 17:23:35 +0530

    offline action

--- offline_action.go
+++ offline_action.go
@@ -6,15 +6,32 @@ type OfflineAction int
 
 const (
 	OfflineActionDisablePage = iota
-
-	OfflineActionDisbleForms
+	OfflineActionDisableForms
+	OfflineActionDoNothing
 )
 
-func getIsConnectedJavascript(
-	action OfflineAction
-) (js string) {
+func getIsConnectedJavascript(action OfflineAction) (js string) {
 	switch action {
-	case OfflineAction:
+	case OfflineActionDisablePage:
+		return `
+		var display = isConnected ? "none" : "flex";
+		document.getElementById("disconnectedoverlay").style.display = display;
+	`
+
+	case OfflineActionDisableForms:
+		return `
+		document.getElementById("disconnectedoverlay").style.display = "none";
+		if (!isConnected) {
+			["input", "button", "textarea", "select"].forEach((selector) => {
+				var matches = document.querySelectorAll(selector);
+				for (var i = 0; i < matches.length; i++) {
+					matches[i].disabled = true;
+				}
+			})
+		}
+	`
+
+	case OfflineActionDoNothing:
 		return ""
 	}
 
